f148dcf780da31fed0854915dacd59efc31b02b3
Alfred Peter <alpeter91@gmail.com>
2023-05-18 23:00:18 +0300

    Add featured project JS html template

--- index.js
+++ index.js
@@ -58,3 +58,35 @@ const feature = {
   source: 'https://github.com/badger-99/Portfolio',
 };
 
+// Featured project template
+const featureTemplate = (featureCard) => `
+  <img class="btm_margin mobile" id="feat_img" src="${
+    featureCard.image1
+  }" alt="image placeholder">
+        <img class="btm_margin desktop" id="feat_img_desktop" src="${
+          featureCard.image2
+        }" alt="image placeholder">
+        <div id="featured_project_right_block">
+          <h3 class="btm_margin" id="featured_project_name">
+            ${featureCard.name}
+          </h3>
+          <p class="btm_margin" id="featured_project_title">
+            ${featureCard.description}
+          </p>
+          <ul class="btm_margin tech_stack">
+            ${featureCard.techStack
+              .map(
+                (tech) => `
+                <li class="tech">
+                  <a class="link" href="">${tech}</a>
+                </li>
+              `
+              )
+              .join('')}
+          </ul>
+          <span class="btm_margin">
+            <a class="link btn see-more" href="">See project</a>
+          </span>
+        </div>
+  `;
+
