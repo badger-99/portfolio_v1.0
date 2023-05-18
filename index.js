9fbb69facfc60d825d0dda57449dbe2662aca164
Alfred Peter <alpeter91@gmail.com>
2023-05-18 22:58:47 +0300

    Add featured-project object literal

--- index.js
+++ index.js
@@ -43,3 +43,18 @@ menuLink.addEventListener('click', () => {
   mobileContainer.style.display = 'none';
   mobileContainer.classList.remove('visible');
 });
+
+// ************************* Dynamic HTML for Featured Project ************************* //
+
+// Featured projet object
+const feature = {
+  name: 'Multi-Post Stories',
+  description:
+    "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+  image1: './images/img.svg',
+  image2: './images/img-desktop.svg',
+  techStack: ['CSS', 'HTML', 'Bootstrap', 'Ruby'],
+  live: 'https://badger-99.github.io/Portfolio/',
+  source: 'https://github.com/badger-99/Portfolio',
+};
+
